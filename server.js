560669ed743948f83a131dbbdbfa73101a28fa41
VINCE-CTS <47523234+VINCE-CTS@users.noreply.github.com>
2020-04-08 11:30:56 +0000

    Kind of description convention for all routes

--- server.js
+++ server.js
@@ -7,5 +7,8 @@ app.use(express.json({ extended: false }))
 
 app.get('/', (req, res) => res.send('API RUNNING !!!'))
 
+// Routes
+app.use('/api/users', require('./routes/users'))
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`APP LISTENING ON PORT ${PORT}`))
